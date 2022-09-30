c8cb4de26888c5fe8c0e8aa2843deff79fd38a28
Ekpen24 <111384393+Ekpen24@users.noreply.github.com>
2022-09-30 05:33:09 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -4,8 +4,8 @@ sonarqubeScanner({
   serverUrl: 'http://34.228.14.44:9000/',    
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
+	    'sonar.projectName': 'Node JS Application - Sample',  
+	    'sonar.projectKey':'nodejsekpen',                                                                      //NodeJsLandmarkTechnologies
 	    'sonar.login': '19b3239c4705d811eb7649a45be29ced8c25a5cc',   
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
